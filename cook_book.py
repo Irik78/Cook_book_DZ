891b17ca206df5d52aa973f2c442eb01842f7ec4
Neto test <muryasov.irik@yandex.ru>
2024-05-09 20:17:35 +0500

    Initial commit

--- cook_book.py
+++ cook_book.py
@@ -7,6 +7,6 @@ with open('cook.txt', encoding='utf-8') as file:
         for x in range(quantity_ingred):
             ingredient_name, quantity, measure = file.readline().split(' | ')
             ingredients.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure':measure})
-        cook_book[name_dish] = ingredients
         file.readline()
-        print(cook_book)
+        cook_book[name_dish] = ingredients
+    print(cook_book)
